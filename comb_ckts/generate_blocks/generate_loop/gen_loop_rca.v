ebfa382766fa0ec44c27128cb6b2c48f965d86f3
shubham pandey <68396186+spdy1895@users.noreply.github.com>
2020-10-04 13:31:42 +0530

    generate loop elaboration.

---
+++ comb_ckts/generate_blocks/generate_loop/gen_loop_rca.v
@@ -0,0 +1,29 @@
+//module for ripple carry adder using generate loop
+module gen_loop_rca (output wire [3:0]sum,
+                    output wire carry_out,
+                    input wire [3:0] in1, in2,
+                    input wire carry_in
+    
+);
+
+parameter N=4;
+
+wire [N:0] carry;
+
+assign carry_out= carry[N];
+
+genvar i;
+
+generate
+    for (i=0 ;i<4 ;i=i+1 ) begin: r_loop
+    wire t1, t2, t3;
+    xor g1(t1, in1[i], in2[i]);
+    xor g2(sum[i], t1, carry[i]);
+    and g3(t2, in1[i], in2[i]);
+    and g4(t3, t1, carry[i]);
+    or g5(carry[i+1], t2, t3);        
+    end
+endgenerate
+assign carry[0]= carry_in;
+
+endmodule //gen_loop_rca
