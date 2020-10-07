80960ccb030660cc87df9961de85b5a3d1f6bca3
shubham pandey <68396186+spdy1895@users.noreply.github.com>
2020-10-07 17:49:01 +0530

    master-slave dff structural description.

---
+++ sequential_ckts/d_flip_flop/dff_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps 
+`include "dff.v"
+module dff_tb (
+    
+);
+wire q, qbar;
+reg d, clk, clr;
+
+dff d1(q, qbar, d, clk, clr);
+
+initial begin
+    clk= 1'b0;
+    forever #5 clk= ~clk;
+end
+
+initial begin
+    $dumpfile("dff_tb.vcd");
+    $dumpvars(0, dff_tb);
+
+    clr= 1'b1; #29;
+    clr= 1'b0; d= 1'b1; #30; // set flip flop
+    d= 1'b0; #30; //reset flip flop
+
+    $finish;
+
+end
+
+endmodule //dff_tb
