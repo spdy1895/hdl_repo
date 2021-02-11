72be06090ba479f1541d62b88055d92e5d194ddc
Shubham Pandey <shubhampandey18061995@gmail.com>
2021-02-11 11:03:13 +0530

    ShubhamP: converted file from dos to unix format

--- sequential_ckts/initializing_memory_from_file/memory_file.v
+++ sequential_ckts/initializing_memory_from_file/memory_file.v
@@ -1,7 +1,7 @@
 //>hdl module to write memory from
 //*an existing file
 module memory_file (
-    
+
 );
 reg [7:0] memory [7:0];
 //declare an 8 byte memory
@@ -13,7 +13,7 @@ initial begin
     $readmemb("memory.mem", memory);
     //display the content of memory
     for(i=0; i<8; i=i+1)
-    $display("Memory[%0d]= %b", i, memory[i]);  
+    $display("Memory[%0d]= %b", i, memory[i]);
     // %0dsupports minimum width
 end
 endmodule //memory_file
